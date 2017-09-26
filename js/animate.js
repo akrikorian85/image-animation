efd9bd537800d68e31b7dca64fee5bd1a7dadb62
Alex Krikorian <akrikorian@envivent.com>
2017-09-26 12:18:47 -0700

    move project to develop

---
+++ js/animate.js
@@ -0,0 +1,131 @@
+var Animation = function (options) {
+  var frames = [];
+  var index = 0;
+  var containerWidth = options.width;
+  var containerHeight = options.height;
+
+  // generates and appends the HTML
+  function appendHTML() {
+    var containerEl = document.createElement('DIV');
+    console.dir(frames);
+
+    $(containerEl).css({
+      'position': 'relative',
+      'width': options.width,
+      'height': options.height
+    });
+
+    frames.forEach(function (frame) {
+      var div = document.createElement('DIV');
+
+      div.id = frame.id;
+      $(div).css(frame.cssProps);
+      $(containerEl).append(div);
+    });
+
+    $('body').append(containerEl);
+  }
+
+  function guid() {
+    function s4() {
+      return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+      s4() + '-' + s4() + s4() + s4();
+  }
+
+  // returns an animation function to run depending on the type param
+  var getAnimationFunc = function (type, duration) {
+    switch (type) {
+      case 'fadeIn':
+        return function (frame) {
+          $('div#' + frame.id).fadeIn(duration, function () {startAnimation(frames[index++])});
+        };
+      case 'reveal':
+        return function (frame) {
+          $('div#' + frame.id).show();
+          $('div#' + frame.id).animate({'width': frame.initialWidth}, duration, function () {startAnimation(frames[index++])});
+        };
+      default: return function (frame) {
+        $('div#' + frame.id).show(0, function () {startAnimation(frames[index++])});
+      }
+    }
+  }
+
+  var startAnimation = function () {
+    if (frames[index]) {
+      frames[index].animation(frames[index])
+    }
+  }
+
+  // returns CSS properties object depending on the animation
+  // frameNum is just to set the z-index. Each frame has a higher z-index than
+  // the frame preceding it
+  var getInitialCssProps = function (frame, frameNum) {
+    var props = {
+      'background-image': 'url(' + frame.imagesrc + ')',
+      'background-size': frame.width + 'px ' + frame.height + 'px',
+      'background-repeat': 'no-repeat',
+      'position': 'absolute',
+      'width': frame.width,
+      'height': frame.height,
+      'top': frame.position.y,
+      'left': frame.position.x,
+      'zIndex': frameNum + 1,
+      'display': 'none'
+    };
+
+    switch (frame.animationType) {
+      case 'fadeIn':
+        break;
+      case 'reveal':
+        props['width'] = 0;
+        break;
+      default:
+    }
+
+    return props;
+  };
+
+  return {
+    // initializer.
+    // loads the image first, then pushes the frame to the frames array
+    'init': function () {
+      var loaded = 0,
+          totalImages = options.frames.length;
+
+      for (var i = 0; i < totalImages; i++) {
+        var image = new Image();
+
+        // preload images
+        image.addEventListener('load', function () {
+          // add options
+          frames.push({
+            'id': guid(),
+            'src': options.frames[loaded].imagesrc,
+            'duration': options.frames[loaded].duration,
+            'animation': getAnimationFunc(options.frames[loaded].animationType, options.frames[loaded].duration),
+            'cssProps': getInitialCssProps(options.frames[loaded], loaded),
+            'initialWidth': options.frames[loaded].width
+          });
+
+          loaded++;
+
+          if (loaded === totalImages) {
+            console.log('starting Animation');
+            appendHTML();
+            startAnimation();
+          } else {
+            console.log(loaded + '/' + totalImages);
+          }
+        });
+
+        image.src = options.frames[i].imagesrc;
+      }
+    }
+  };
+};
+
+
---
+++ js/main.js
@@ -0,0 +1,73 @@
+$(function () {
+  var options = {
+    'width': 813,
+    'height': 614,
+    'frames': [
+      {
+        'imagesrc': 'images/chart-bg.jpg',
+        'position': {
+          x: 0,
+          y: 0
+        },
+        'width': 813,
+        'height': 614,
+        'animationType': 'none',
+        'duration': 1000
+      }, {
+        'imagesrc': 'images/gray-line-1.png',
+        'position': {
+          x: 164,
+          y: 94
+        },
+        'width': 221,
+        'height': 212,
+        'animationType': 'reveal',
+        'duration': 1000
+      }, {
+        'imagesrc': 'images/orange-line-1.png',
+        'position': {
+          x: 164,
+          y: 90
+        },
+        'width': 221,
+        'height': 125,
+        'animationType': 'reveal',
+        'duration': 1000
+      }, {
+        'imagesrc': 'images/2-lines.png',
+        'position': {
+          x: 385,
+          y: 214
+        },
+        'width': 244,
+        'height': 153,
+        'animationType': 'reveal',
+        'duration': 1000
+      }, {
+        'imagesrc': 'images/gray-explanation.png',
+        'position': {
+          x: 170,
+          y: 321
+        },
+        'width': 179,
+        'height': 100,
+        'animationType': 'fadeIn',
+        'duration': 1000
+      }, {
+        'imagesrc': 'images/orange-explanation.png',
+        'position': {
+          x: 634,
+          y: 150
+        },
+        'width': 179,
+        'height': 100,
+        'animationType': 'fadeIn',
+        'duration': 1000
+      }
+    ]
+  };
+
+  var a = new Animation(options);
+
+  a.init();
+});
